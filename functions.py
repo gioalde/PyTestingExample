767140010309973421392bf829bc944ad8f17eba
Giovanni Aldegheri <gioalde@dtu.dk>
2026-03-26 11:30:51 +0100

    Add validation to convert_fahrenheit_to_celsius for absolute zero

--- functions.py
+++ functions.py
@@ -11,4 +11,6 @@ def multiply(a, b):
 
 
 def convert_fahrenheit_to_celsius(fahrenheit):
+    if fahrenheit < -459.67:
+        raise AssertionError("Temperature cannot be below absolute zero.")
     return multiply(subtract(fahrenheit, 32), 5 / 9)
