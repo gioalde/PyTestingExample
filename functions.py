ab56f293d496f5c54ede3dd62f11577fe3087448
Giovanni Aldegheri <gioalde@dtu.dk>
2026-03-26 11:37:25 +0100

    Add factorial function and corresponding tests

--- functions.py
+++ functions.py
@@ -14,3 +14,15 @@ def convert_fahrenheit_to_celsius(fahrenheit):
     if fahrenheit < -459.67:
         raise AssertionError("Temperature cannot be below absolute zero.")
     return multiply(subtract(fahrenheit, 32), 5 / 9)
+
+
+def factorial(n):
+    """
+    Computes the factorial of n.
+    """
+    if n < 0:
+        raise ValueError('received negative input')
+    result = 1
+    for i in range(1, n + 1):
+        result *= i
+    return result
--- test_functions.py
+++ test_functions.py
@@ -1,5 +1,6 @@
-from functions import add, subtract, multiply
+from functions import add, factorial, subtract, multiply
 from functions import convert_fahrenheit_to_celsius as f2c
+from functions import factorial
 import pytest
 
 
@@ -17,3 +18,10 @@ def test_convert_fahrenheit_to_celsius():
    assert f2c(122) == pytest.approx(50)
    with pytest.raises(AssertionError):
        f2c(-600)
+
+def test_factorial():
+    assert factorial(0) == 1
+    assert factorial(1) == 1
+    assert factorial(2) == 2
+
+    
